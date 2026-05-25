b3d0edc4bae71a3285943d408e1b500b0d924fa8
ANANTA AKBAR PRATAMA <you@example.com>
2026-05-25 13:42:22 +0700

    Menyempurnakan opsi SSL dan mem-bypass validasi server

--- config/database.php
+++ config/database.php
@@ -61,7 +61,8 @@ return [
             'engine' => null,
             'options' => [
                 PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-            ], 
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ],
         ],
 
         'mariadb' => [
